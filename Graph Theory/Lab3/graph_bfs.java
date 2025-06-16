7013b92789d41b14aa3fdfa914e323acd66e7011
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-06-16 08:49:47 +0530

    l3 init

---
+++ Graph Theory/Lab3/graph_bfs.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class graph_bfs {
+    public static void main(String[] args) {
+        int adj[][] = {{1,2},{0,2,3},{0,1,4},{1,4},{2,3}};
+
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> li = new ArrayList<>();
+        boolean[] visited = new boolean[adj.length];
+
+        q.offer(0);
+        visited[0] = true;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            li.add(node);
+            //for bfs, will check the neighbor node
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.offer(neighbor);
+                }
+            }
+        }
+        System.out.println(li);
+    }
+}
