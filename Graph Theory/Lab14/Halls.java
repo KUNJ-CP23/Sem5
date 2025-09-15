ec51b083619e0ae0d80bf671db333321c25a52a9
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-09-15 09:29:12 +0530

    something

---
+++ Graph Theory/Lab14/Halls.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Halls {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //taking static bipartite graph input
+        int[][] graph = {{0,0},{1,0},{2,1},{3,2}};
+        int n = graph.length;
+        int m = graph[0].length;
+         
+        sc.close();
+    }
+}
